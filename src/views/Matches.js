d17cb46357c04324fcdfe96b8f8fb981a49ff2be
Sepci0 <ppolgon@gmail.com>
2021-01-15 18:03:31 +0100

    chore: export chore

--- src/views/Matches.js
+++ src/views/Matches.js
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const Matches= () => {
+export default () => {
   return (
     <div className="container">
       <h1>Home!!</h1>
     </div>
   );
 }
-
-export default Matches;

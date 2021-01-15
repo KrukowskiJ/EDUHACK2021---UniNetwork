de589c5191e58437c942b1e6dcdf2895b2bda691
Sepci0 <ppolgon@gmail.com>
2021-01-15 17:55:12 +0100

    feat: project listing

--- src/views/ProjectList.js
+++ src/views/ProjectList.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
 export default () => {
+
+  let data = [{ a: 2 }];
+  // fetch for projects
+  // fetch('http://example.com/movies.json')
+  //   .then(response => response.json())
+  //   .then(data => console.log(data));
+
+  const listItems = data.map((obj) =>
+    <li>{obj.a}</li>
+  );
+
+
   return (
     <div className="container">
       <h1>ProjectList</h1>
+      <ul>
+        {listItems}
+      </ul>
     </div>
   );
 }

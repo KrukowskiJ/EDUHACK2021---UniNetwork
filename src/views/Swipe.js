8821a98c7a92b129b84f8ca691a40dc0088eb8f0
Sepci0 <ppolgon@gmail.com>
2021-01-15 20:01:33 +0100

    wip

--- src/views/Swipe.js
+++ src/views/Swipe.js
@@ -1,13 +1,45 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 import styled from "styled-components"
+import SectionTitle from "./atoms/SectionTitle";
 
 export default () => {
   return (
     <>
-      <Title>Swipe</Title>
+      <SectionTitle title="Swipe" />
+
       <GreenBox />
       <CardBorder>
-        content
+        <Container>
+          <Row>
+            <Col>
+              <Row>
+                <Col xs={4}> Prj logo</Col>
+                <Col>
+                  Prj name
+                  Prj name
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  University
+                  <br></br>Wat najlepsza uczelnia jest
+                </Col>
+              </Row>
+              <Row>
+                <Col>
+                  Like
+                </Col>
+                <Col>
+                  Dislike
+                </Col>
+              </Row>
+            </Col>
+            <Col>
+              prj desc
+            </Col>
+          </Row>
+        </Container>
       </CardBorder>
     </>
   );
@@ -15,11 +47,11 @@ export default () => {
 
 const CardBorder = styled.div`
 border:solid 5px #4C6C84;
-border-top: solid 20px #ACF2D3;
+/* border-top: solid 20px #ACF2D3;  this looks wierd idk why*/
 
 `
 const GreenBox = styled.div`
-
+background-color:#ACF2D3;
 
 `
 const Title = styled.div`

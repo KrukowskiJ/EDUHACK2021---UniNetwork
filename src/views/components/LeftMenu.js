35a2d41902fb8f1d626431a92d8b65e8e3cd782e
Adam Pawelek <adam.pwk@outlook.com>
2021-01-15 20:59:47 +0200

    moje zmiany

--- src/views/components/LeftMenu.js
+++ src/views/components/LeftMenu.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import styled from 'styled-components'
+
+
+const Content = styled.div`
+    height: 92vh;
+    width: 100%;
+    color: red;
+    background: #D0D0D0;
+    margin: 0;
+    
+`;
 
 export default () => {
   return (
-    <div className="container">
+    <Content>
       <h1>Menu</h1>
-    </div>
+    </Content>
   );
 }
 
--- src/views/components/TopMenu.js
+++ src/views/components/TopMenu.js
@@ -1,14 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+
+const Heading = styled.div`
+    height: 8vh;
+    width: 100vw;
+    color: red;
+    background: #4C6C84;
+    margin: 0;
+    
+`;
 
 export default () => {
   return (
-    <div className='container'>
+
+        <Heading>
+
       <h1>temp menu</h1>
       <Link to='projects'>Projects</Link> {" "}
       <Link to='matches'>matches</Link> {" "}
       <Link to='profile'>profile</Link> {" "}
-    </div>
+        </Heading>
   );
 }
 

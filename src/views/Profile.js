88923581d4383a8d7f19cd2e686a6d3e744a66f3
KrukowskiJ <jakub.krukowski.business@gmail.com>
2021-01-16 17:30:56 +0100

    fix user profile card add experience

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -2,66 +2,25 @@ import React from "react";
 import styled from "styled-components";
 import { Row, Col } from "react-bootstrap";
 import SkillBox from "./atoms/SkillBox";
-import SkillBoxEmpty from "./atoms/SkillBoxEmpty";
 import SkillsPng from "../img/profile_skills.png";
 import SectionTitle from "./atoms/SectionTitle";
-import Photo from "../img/girl.png";
+import Photo from "../img/zdjecie1.png";
+import userData from "../data/users.json";
+import prjData from "../data/projects.json";
+
 export default () => {
   return (
     <div>
-      <br></br>
-      <br></br>
       <div>
         <SectionTitle title="PROFILE" />
       </div>
-      <GreenLine />
-      <Box>
-        <Row>
-          <Col md="6">
-                <Row>
-                  <Col md="6">
-                     <Avatar src={Photo} />
-                  </Col>
-                  <Col md="6">
-                    <SmallTittle>NAME</SmallTittle>
-                    <NameAndSurname>ANIA</NameAndSurname>
 
-                    <SmallTittle>SURNAME</SmallTittle>
-                <NameAndSurname>KOWALSKA</NameAndSurname>
-                  </Col>
-            </Row>
-            <br></br>
-            <SmallTittle>UNIVERSITY</SmallTittle>
-            <Univesity>MILITARY UNIVERSITY OF TECHNOLOGY</Univesity>
-            <br></br>
-            <BigTittle>MY RATING</BigTittle>
-            <SkillsImg src={SkillsPng} />
-          </Col>
-          <Col md="6">
-            <BigTittle>ABOUT ME</BigTittle>
-            <Description>
-              I am a new studnet, i can program a bit, and i am interested in
-              biology. I would like to participate in small project and develop
-              my skills. I have already participated in my school project eco
-              world, when we coded a simple app that help people witg
-              segregation of rubbish Any mentorgng would be great, messege me
-              for more info!
-            </Description>
-            <BigTittle>SKILLS</BigTittle>
-            <Row>
-              <SkillBox skill="Programming" />
-              <SkillBox skill="Biology" />
-              <SkillBox skill="Project Managment" />
-            </Row>
-            <BigTittle>EXPIRIENCE</BigTittle>
-          </Col>
-        </Row>
-      </Box>
+      <Profile user={userData[0]} project={prjData[0]} />
     </div>
   );
 };
 
-const Avatar=styled.img`
+const Avatar = styled.img`
   height:200px;
   width:200px;
 `
@@ -76,10 +35,9 @@ const SmallTittle = styled.h1`
   margin-top:20px;
   font-family: Roboto;
   font-style: normal;
-  font-weight: 100;
   font-size: 20px;
   line-height: 29px;
-
+  font-weight: bold;
   color: #7a7a7a;
 `;
 
@@ -88,6 +46,7 @@ const BigTittle = styled.h1`
   font-weight: 100;
   font-size: 25px;
   line-height: 29px;
+  margin-top: 15px;
   color: #4fb084;
 `;
 
@@ -129,10 +88,68 @@ const SkillsImg = styled.img`
 `;
 
 const Univesity = styled.h1`
-  font-family: Roboto;
+margin-top: 30px;
+   font-family: Roboto;
   font-style: normal;
-  font-weight: bold;
-  font-size: 18px;
-  line-height: 21px;
-  color: #4fb084;
+  font-weight: normal;
+  font-size: 35px;
+  line-height: 16px;
+  color: #989898;
 `;
+
+const Date = styled.h1`
+    font-family: Roboto;
+    font-style: normal;
+    font-weight: bold;
+    font-size: 14px;
+    line-height: 16px;
+    color: #7A7A7A;
+`
+const Profile = ({ user, project }) => {
+  return (
+    <GreenLine >
+      <Box>
+        <Row>
+          <Col md="6">
+            <Row>
+              <Col md="6">
+                <Avatar src={Photo} />
+              </Col>
+              <Col md="6">
+                <SmallTittle>NAME</SmallTittle>
+                <NameAndSurname> {user.name}</NameAndSurname>
+
+                <SmallTittle>SURNAME</SmallTittle>
+                <NameAndSurname>{user.surname}</NameAndSurname>
+              </Col>
+            </Row>
+            <br></br>
+            <BigTittle>UNIVERSITY</BigTittle>
+            <Univesity>{user.uni}</Univesity>
+            <br></br>
+            <BigTittle>MY RATING</BigTittle>
+            <SkillsImg src={SkillsPng} />
+          </Col>
+          <Col md="6">
+            <BigTittle>ABOUT ME</BigTittle>
+            <Description>
+              {user.aboutMe}
+            </Description>
+            <BigTittle>SKILLS</BigTittle>
+            <Row>
+              <SkillBox skill="Programming" />
+              <SkillBox skill="Biology" />
+              <SkillBox skill="Project Managment" />
+            </Row>
+            <BigTittle>EXPERIENCE</BigTittle>
+            <SmallTittle>{project.name}</SmallTittle>
+            <Description>
+              {project.shortDesc}
+            </Description>
+            <Date>{project.date}</Date>
+          </Col>
+        </Row>
+      </Box>
+    </GreenLine>
+  );
+}

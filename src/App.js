2b40dc863a4cf1c9c8143fe4e30310c13ad79f90
Sepci0 <ppolgon@gmail.com>
2021-01-16 13:14:25 +0100

    login fix

--- src/App.js
+++ src/App.js
@@ -10,6 +10,8 @@ import ProjectList from './views/ProjectList';
 import Swipe from './views/Swipe';
 import TopMenu from "./views/components/TopMenu";
 import LeftMenu from "./views/components/LeftMenu";
+import Login from "./views/Login"
+
 import { Container, Row, Col } from 'react-bootstrap'
 import PerfectScrollbar from 'react-perfect-scrollbar'
 import AddProject from './views/AddProject'
@@ -17,22 +19,21 @@ import AddProject from './views/AddProject'
 function App() {
   return (
     <Router>
+      <Route exact path="/" component={Login} />
       <TopMenu />
       <Container fluid={true} >
         <Row>
           <Col xs={3} style={{ padding: 0 }}>
             <LeftMenu />
           </Col>
-          <Col xs={7}>
+          <Col xs={9}>
             <div className="container">
-              <Route exact path="/" component={Swipe} />
               <Route path="/projects" component={ProjectList} />
               <Route exact path="/matches" component={Matches} />
               <Route path="/profile" component={Profile} />
               <Route path="/swipe" component={Swipe} />
               <Route path="/addProject" component={AddProject} />
             </div>
-
           </Col>
         </Row>
       </Container>
